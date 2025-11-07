b302235203985a588f4735231a5f8114b6962bdf
Tamanna@5092 <tamannaaktermoni51@gmailcom>
2025-11-07 11:45:33 +0600

    tic tac toe board setup

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    console.log("click");
+    setValue("X");
+  }
+
+  return (
+    <button
+      onClick={handleClick}
+      className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
